fa8355fa33d3bcc15d07bc124ac779ee3ed9d4c3
zac-hills <codinfreakgf@gmail.com>
2020-01-28 21:39:13 -0500

    <Added><Better box shadow>

--- src/components/Page/BioPage.jsx
+++ src/components/Page/BioPage.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import Footer from "../Footer/Footer";
 import "./biopage.css";
+import TextBlob from "../TextBlob/TextBlob";
 
 class BioPage extends Component {
   state = {};
   render() {
+
+
     return (
       <article className="contentfield">
         <img
@@ -12,15 +15,16 @@ class BioPage extends Component {
           src="/ProfilePicture.jpg"
           width={200}
           height={200}
+          padding={"3px"}
         />
-        <h1>Interests</h1>I would say my background is more centered around
+
+        <TextBlob width="20%" padding="35px" title="Interests" left="60px" text="I would say my background is more centered around
         general programming. The task of picking a specific field is often
         difficult when you spend your time flipping from one topic to another.
         There is one topic that distinguishes itself from the other topics and
         that is tool design. The process of automating difficult or frequent
-        mundane tasks is the reason I get out of bed in the morning.
-        <hr />
-        <h1>Education</h1>
+        mundane tasks is the reason I get out of bed in the morning."/>
+        <TextBlob width="20%" padding="35px" title="Education" right="60px" text="
         I have a Bachelors in Information Technology with a major in Game
         Development. During my bachelors my primary focus was to develop my
         skills as an engine developer. I found early on that I liked creating
@@ -28,8 +32,7 @@ class BioPage extends Component {
         fact that the one commodity humans always need is time. Preceeding my
         bachelors I went on to pursure a Masters in Computer Science with a
         focus in Computer Vision. My previous background in game development
-        built the foundation working with the GPU and images.
-        <hr />
+        built the foundation working with the GPU and images."/>
         <Footer />
       </article>
     );
