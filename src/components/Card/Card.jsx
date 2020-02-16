db1dfccb121c70d32e63dfecb44ed5a8250102b4
zachary-hills <zachary.hills@uoit.net>
2020-02-16 15:09:58 -0500

    eod

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,41 +1,72 @@
-import * as React from 'react';
+import * as React from "react";
 import "./Card.css";
- 
+
 class Card extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {skew:{x:0, y:0}};
+  constructor(props) {
+    super(props);
+    this.state = { transform: "skew(0deg,0deg)" };
+  }
+
+  onHover(e) {
+    console.log(e);
+    const maxSkew = 5;
+    let x = this.state.size.x + this.state.size.width / 2;
+    let y = this.state.size.y + this.state.size.height / 2;
+    let xDiff = x - e.screenX;
+    let yDiff = y - e.screenY;
+    xDiff /= this.state.size.width / 2;
+    yDiff /= this.state.size.height / 2;
+    this.setState({
+      transform: `skew(${Math.floor(xDiff * maxSkew)}deg,${Math.floor(
+        yDiff * maxSkew
+      )}deg)`
+    });
+  }
+  onClick() {
+    if (this.props.url != null) {
+      // location.href = this.props.url;
+    } else {
+      console.log("this card does not have url");
     }
-    onHover(){
-        console.log(this.state.size);
-    }
-    onClick(){
-        if(this.props.url !=null)
-        {
-           // location.href = this.props.url;
-        }
-        else{
-            console.log("this card does not have url");
-        }
-    }
-    componentDidMount(){
-        this.setState({size:this.element.getBoundingClientRect()});
-    }
-    getSize(element){
-        this.element=element;
-    }
-    render() { 
-        return (<div onClick={this.onClick.bind(this)} onMouseOver={this.onHover.bind(this)} ref={this.getSize.bind(this)}className="card" style={{width: this.props.width, height: this.props.height, transform:`skew(${this.state.skew.x},${this.state.skew.y})`}}>
-            <div style={{width:"100%", height:"80%", display:"block"}}>
-                <img src={this.props.src}></img>
-            </div>
-            <div style={{width:"100%", height:"19%", display:"block"}}>
-                <p style={{fontFamily: this.props.fontFamily, fontSize: this.props.fontSize}}>
-                    {this.props.text}
-                </p>
-            </div>
-        </div>);
-    }
+  }
+  componentDidMount() {
+    this.setState({ size: this.element.getBoundingClientRect() });
+  }
+  getSize(element) {
+    this.element = element;
+  }
+  shouldComponentUpdate(nextProps) {
+    return this.state.transform !== nextProps.transform;
+  }
+  render() {
+    return (
+      <div
+        onClick={this.onClick.bind(this)}
+        onMouseMove={this.onHover.bind(this)}
+        ref={this.getSize.bind(this)}
+        className="card"
+        style={{
+          width: this.props.width,
+          height: this.props.height,
+          transform: this.state.transform
+        }}
+      >
+        <div style={{ width: "100%", height: "80%", display: "block" }}>
+          <img src={this.props.src}></img>
+        </div>
+        <div style={{ width: "100%", height: "19%", display: "block" }}>
+          <p
+            style={{
+              fontFamily: this.props.fontFamily,
+              fontSize: this.props.fontSize
+            }}
+          >
+            {this.props.text}
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Card;
